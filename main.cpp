a0f52af522f5d8d203ea0463af8f9ea173389c50
jisookim <jisookimwork@gmail.com>
2024-05-24 10:05:44 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+	srand(time(Null)); // 난수 발생을 위한 시드 설정 
+
+	int Size = 10; 
+	int* Numbers = new int[Size];
+
+	int Num; 
+
+	for (int i = 0; i < Size; i++)
+	{
+		Numbers[i] = rand() % 100 + 1; 
+	}
+
+
+
+
+
+	return 0;
+}
