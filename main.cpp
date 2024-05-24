25923eecd8aadda956431b7777026fcd88a68a60
jisookim <jisookimwork@gmail.com>
2024-05-24 10:45:48 +0900

    아우 잠와

--- main.cpp
+++ main.cpp
@@ -2,19 +2,46 @@
 
 using namespace std; 
 
+
+void InsertAfter(Int* Array, Int Size, Int NewPosition, Int Value )
 int main()
 {
-	srand(time(Null)); // 난수 발생을 위한 시드 설정 
-
 	int Size = 10; 
-	int* Numbers = new int[Size];
-
-	int Num; 
-
-	for (int i = 0; i < Size; i++)
+	int* PA = new int[10]; 
+	
+	for (int i = 0; i <= 10; i++)
 	{
-		Numbers[i] = rand() % 100 + 1; 
+		PA[i] = i;
 	}
+
+	//그대로 옮기는 것 
+	int NewPA = new int[Size +1]; 
+	for (int i = 0; i < NewPosition; i++)
+	{
+		NewPA[i] = PA[i]
+	}
+
+	//추가 로직 
+	for (int  i = 0; i < NewPosition; i++)
+	{
+		cout << NewPA[i] << " , ";
+	}
+
+
+	NewPA[NewPosition] = NewValue;
+	for (int i = NewPosition + 1, i < Size + 1; ++i)
+	{
+		NewPA[i] = PA[i - 1]; 
+	}
+
+	for (int i = 0; i < Size + 1; ++1)
+	{
+		cout << PA[i] << " , ";
+	}
+
+	delete[] PA;
+	delete[] NewPA;
+
 
 
 	return 0;
